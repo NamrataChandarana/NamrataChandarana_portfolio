0b78c76bb078f159a98bbd03f7da021d089002d9
Namrata Chandarana <namratachandarana20@gmail.com>
2024-06-22 17:30:40 +0530

    footer section

---
+++ app/(home)/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function 
+() {
+  return (
+    <div>
+        <h1 className=" text-white underline underline-offset-8 decoration-indigo-500 font-bold text-2xl -rotate-2">
+            Namrata 👩🏻‍💻
+        </h1>
+    </div>
+  )
+}
